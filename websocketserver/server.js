c06cc9089771ea13c51f378ab66da58431fa4a1e
Jonas Johansson <jns.johansson@gmail.com>
2019-04-13 10:59:24 +0200

    webpage sends beat

--- websocketserver/server.js
+++ websocketserver/server.js
@@ -1,17 +1,17 @@
-const WebSocket = require('ws')
+const WebSocket = require('ws');
 
-const PORT = 0xF57 /* "FST"=3927 */
-const wss = new WebSocket.Server({ port: PORT })
+const PORT = 0xf57; /* "FST"=3927 */
+const wss = new WebSocket.Server({ port: PORT });
 
-console.log(`serving on port ${PORT}…`)
+console.log(`serving on port ${PORT}…`);
 wss.on('connection', ws => {
-    console.log('connected…')
-    ws.on('message', data => {
-        console.log('received: %s', data);
-        wss.clients.forEach(client => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data.toString('utf8'))
-            }
-        })
-    })
-})
+	console.log('connected…');
+	ws.on('message', data => {
+		console.log('received: %s', data);
+		wss.clients.forEach(client => {
+			if (client !== ws && client.readyState === WebSocket.OPEN) {
+				client.send(data.toString('utf8'));
+			}
+		});
+	});
+});
