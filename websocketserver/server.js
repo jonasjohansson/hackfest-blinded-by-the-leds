3b7010cfe04cd4fde6db8685fc7b015469be862c
Jonas Johansson <jns.johansson@gmail.com>
2019-04-13 17:33:20 +0200

    add message counter for debug

--- websocketserver/server.js
+++ websocketserver/server.js
@@ -3,11 +3,13 @@ const WebSocket = require('ws');
 const PORT = 0xf57; /* "FST"=3927 */
 const wss = new WebSocket.Server({ port: PORT });
 
+var numMessages = 0;
+
 console.log(`serving on port ${PORT}…`);
 wss.on('connection', ws => {
 	console.log('connected…');
 	ws.on('message', data => {
-		console.log('received: %s', data);
+		console.log('received: %s', data, numMessages++);
 		wss.clients.forEach(client => {
 			if (client !== ws && client.readyState === WebSocket.OPEN) {
 				client.send(data.toString('utf8'));
